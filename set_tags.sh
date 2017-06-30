bed533b60d5ce9ef2b53da51ff199deba8906ea6
Vinetos <Vinetos.tutoriel@gmail.com>
2017-06-30 10:25:45 +0200

    Rename output apk and change versioning model [skip ci]

--- set_tags.sh
+++ set_tags.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       git config --global user.name "Travis"
 
       # Add tag and push to master.
-      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+      git tag -a v0.${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       git push origin --tags
       git fetch origin
 
