73d371ae1e97d5405eae9e2c0f6e714b61c9c52a
Vinetos <Vinetos.tutoriel@gmail.com>
2017-08-06 19:29:51 +0200

    Fix versioning

--- set_tags.sh
+++ set_tags.sh
@@ -8,18 +8,18 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
 
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
-      echo -e "Starting to tag commit.\n"
+      echo "Starting to tag commit.\n"
 
       git config --global user.email "travis@travis-ci.org"
       git config --global user.name "Travis"
 
       # Add tag and push to master.
       . app/version.properties
-      git tag -a -m  "Travis build $TRAVIS_BUILD_NUMBER pushed a tag." "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH ($VERSION_BUILD)"
+      git tag -a -m  "Travis build $TRAVIS_BUILD_NUMBER pushed a tag." "v$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH ($VERSION_BUILD)"
       git push origin --tags
       git fetch origin
 
-      echo -e "Done magic with tags.\n"
+      echo "Done magic with tags.\n"
   fi
   fi
 fi
