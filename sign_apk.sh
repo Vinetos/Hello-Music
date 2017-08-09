7e360766ff890a1bd222197d40e7b9e495bff23e
Vinetos <Vinetos.tutoriel@gmail.com>
2017-08-09 14:50:05 +0200

    Auto sign apk with the new BuildTools version

--- sign_apk.sh
+++ sign_apk.sh
@@ -1,7 +1,7 @@
 NAME="HelloMusic-$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-build.$VERSION_BUILD-$VERSION_NUMBER"
 
-zipalign -v -p 4 "$NAME.apk" "$NAME-aligned.apk"
+${ANDROID_HOME}/build-tools/26.0.1/zipalign -v -p 4 "apk/$NAME.apk" "apk/$NAME-aligned.apk"
 
-apksigner sign --ks HelloMusic_Keystore.jks --out "$NAME-release.apk" "$NAME-aligned.apk"
+${ANDROID_HOME}/build-tools/26.0.1/apksigner sign --ks HelloMusic_Keystore.jks --ks-pass "pass:$storepass" --key-pass "pass:$keypass" --out "apk/$NAME-release.apk" "apk/$NAME-aligned.apk"
 
-apksigner verify "$NAME-release.apk"
+${ANDROID_HOME}/build-tools/26.0.1/apksigner verify "$NAME-release.apk"
