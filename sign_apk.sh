95de4e3e632f54ee0c79650b75a30c1cb2acbb63
Vinetos <Vinetos.tutoriel@gmail.com>
2017-08-09 14:56:57 +0200

    Fix path of built apks

--- sign_apk.sh
+++ sign_apk.sh
@@ -1,7 +1,9 @@
 NAME="HelloMusic-$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-build.$VERSION_BUILD-$VERSION_NUMBER"
 
-${ANDROID_HOME}/build-tools/26.0.1/zipalign -v -p 4 "apk/$NAME.apk" "apk/$NAME-aligned.apk"
+cd ./apk/
 
-${ANDROID_HOME}/build-tools/26.0.1/apksigner sign --ks HelloMusic_Keystore.jks --ks-pass "pass:$storepass" --key-pass "pass:$keypass" --out "apk/$NAME-release.apk" "apk/$NAME-aligned.apk"
+${ANDROID_HOME}/build-tools/26.0.1/zipalign -v -p 4 "$NAME.apk" "$NAME-aligned.apk"
+
+${ANDROID_HOME}/build-tools/26.0.1/apksigner sign --ks HelloMusic_Keystore.jks --ks-pass "pass:$storepass" --key-pass "pass:$keypass" --out "$NAME-release.apk" "$NAME-aligned.apk"
 
 ${ANDROID_HOME}/build-tools/26.0.1/apksigner verify "$NAME-release.apk"
