fd9b4e32f98efd95854f1533f0b9efed7527c563
Vinetos <Vinetos.tutoriel@gmail.com>
2017-08-09 15:27:01 +0200

    Copy release to publish wih tag

--- sign_apk.sh
+++ sign_apk.sh
@@ -6,8 +6,10 @@ cd ./apk/
 
 ${ANDROID_HOME}/build-tools/26.0.1/zipalign -v -p 4 "$NAME.apk" "$NAME-aligned.apk"
 
-mkdir -p ../release
-
-${ANDROID_HOME}/build-tools/26.0.1/apksigner sign --ks ../HelloMusic_Keystore.jks --ks-pass "pass:$storepass" --key-pass "pass:$keypass" --out "../release/$NAME-release.apk" "$NAME-aligned.apk"
+${ANDROID_HOME}/build-tools/26.0.1/apksigner sign --ks ../HelloMusic_Keystore.jks --ks-pass "pass:$storepass" --key-pass "pass:$keypass" --out "$NAME-release.apk" "$NAME-aligned.apk"
 
 ${ANDROID_HOME}/build-tools/26.0.1/apksigner verify "$NAME-release.apk"
+
+cd ../
+mkdir release
+cp "./apk/$NAME-release.apk" "./release/$NAME-release.apk"
